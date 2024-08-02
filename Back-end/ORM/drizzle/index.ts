53e22ac4453c028c458b446507d411ee3b7c95e1
Sarmad rafique <sarmadrafique040@gmail.com>
2024-08-02 13:12:28 +0500

    Removed: insert operation

--- Back-end/ORM/drizzle/index.ts
+++ Back-end/ORM/drizzle/index.ts
@@ -6,22 +6,6 @@ const queryConnection = postgres(process.env.DATABASE_URL!);
 
 const db = drizzle(queryConnection, { schema });
 
-await db
-    .insert(schema.UserTable)
-    .values(
-        {
-            name: "Nawaz",
-            age: 18,
-            email: "nawaz@email.com",
-        }
-    )
-    .returning({
-        id: schema.UserTable.id,
-    }).onConflictDoUpdate({
-        target: schema.UserTable.email,
-        set: { name: "Updated name" },
-    })
-
 
 const users = await db.query.UserTable.findMany({})
 
